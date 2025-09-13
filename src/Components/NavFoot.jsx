c179b8f8ac3ebf6b60ce3a69e3ed95d777c19dca
Raquel Lais <78700528+Raquellts@users.noreply.github.com>
2025-09-13 11:13:22 -0300

    Update contato.jsx

--- src/Components/NavFoot.jsx
+++ src/Components/NavFoot.jsx
@@ -12,7 +12,7 @@ function NavFoot() {
         <Col>
           <div className="InfosFoot">
             <strong>Contato</strong>
-            <div>(11) 97602 - 3098</div>
+            <div>(11) 99348 - 0155</div>
             <div>joaovitorfernandes9@hotmail.com</div>
           </div>
           <p className="InfosFoot" style={{ margin: "10px 0px 0px -20px" }}>
